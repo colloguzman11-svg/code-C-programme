45366163ca6c4d1a68b0a569948448a488190a94
colloguzman11-svg <collo.guz.man.11@gmail.com>
2025-09-25 14:33:11 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,17 @@
+/* 
+Name: collins kimani
+Reg no: PA106/29036/G/25
+Description: units for this semester
+*/
+#include <stdio.h>
+int main()
+{
+printf("discreet mathematics\n");
+printf("mathematics for science\n");
+printf("fundamentals of ict\n");
+printf("fundamentals of system analysis and design\n");	
+printf("introduction to programming and problem solving\n");
+printf("communication and information literacy skills\n");
+printf("computer systems and organisations\n ");
+	return 0;
+}
