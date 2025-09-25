bad54528ac289ce972c6dc0f3be062cc17cd2bdf
colloguzman11-svg <collo.guz.man.11@gmail.com>
2025-09-25 14:34:08 +0300

    Add files via upload

---
+++ balance.c
@@ -0,0 +1,27 @@
+/*
+name: collins kimani
+reg : PA106/29036/G/25
+Description : height bank balance id number
+*/
+
+#include<stdio.h>s
+
+int main()
+{
+float height;
+int idnumber,accbal;
+printf("Enter your height:");
+scanf("%f", &height);
+printf("enter your acc bal:");
+scanf("%d", &accbal);
+printf("enter your id number:");
+scanf("%d", &idnumber);
+
+//output
+printf("Your height is %.2f\n", height);
+printf("Your account ballance is ksh %d \n", accbal);
+printf("Your ID number is %d\n", idnumber);
+
+
+	return 0;
+}
