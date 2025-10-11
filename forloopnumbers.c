6e9992d45fb7f7178802140649fd61423da59e51
colloguzman11-svg <collo.guz.man.11@gmail.com>
2025-10-12 01:39:09 +0300

    Add files via upload

---
+++ forloopnumbers.c
@@ -0,0 +1,16 @@
+/*
+Name: Collins Kimani
+Reg:  PA106/G/29036/25
+Description: for loop for numbers starting from 100 descending to 50
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int i;
+for (i=100; i>=50; i--){
+	printf("%d\n", i);
+}	
+	return 0;
+}
